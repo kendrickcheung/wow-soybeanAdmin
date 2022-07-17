7ada6b62fe27261adc77454a0fd4eaf320f8177a
kendrick <2016337368@qq.com>
2022-07-17 15:20:51 +0800

    更新一些通用组件

--- src/typings/components.d.ts
+++ src/typings/components.d.ts
@@ -7,15 +7,21 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    BetterScroll: typeof import('./../components/custom/BetterScroll.vue')['default']
+    CountTo: typeof import('./../components/custom/CountTo.vue')['default']
     DarkModeContainer: typeof import('./../components/common/DarkModeContainer.vue')['default']
     DarkModeSwitch: typeof import('./../components/common/DarkModeSwitch.vue')['default']
     ExceptionBase: typeof import('./../components/common/ExceptionBase.vue')['default']
+    GithubLink: typeof import('./../components/custom/GithubLink.vue')['default']
+    HoverContainer: typeof import('./../components/common/HoverContainer.vue')['default']
     IconAntDesignCloseOutlined: typeof import('~icons/ant-design/close-outlined')['default']
     IconAntDesignEnterOutlined: typeof import('~icons/ant-design/enter-outlined')['default']
     IconAntDesignSettingOutlined: typeof import('~icons/ant-design/setting-outlined')['default']
     'IconClarity:notificationLine': typeof import('~icons/clarity/notification-line')['default']
     IconCustomAvatar: typeof import('~icons/custom/avatar')['default']
     IconCustomEmptyData: typeof import('~icons/custom/empty-data')['default']
+    IconCustomLogo: typeof import('~icons/custom/logo')['default']
+    IconCustomLogoFill: typeof import('~icons/custom/logo-fill')['default']
     IconCustomNetworkError: typeof import('~icons/custom/network-error')['default']
     IconCustomNoPermission: typeof import('~icons/custom/no-permission')['default']
     IconCustomNotFound: typeof import('~icons/custom/not-found')['default']
@@ -41,6 +47,7 @@ declare module '@vue/runtime-core' {
     IconPhCaretDoubleRightBold: typeof import('~icons/ph/caret-double-right-bold')['default']
     IconSelect: typeof import('./../components/custom/IconSelect.vue')['default']
     IconUilSearch: typeof import('~icons/uil/search')['default']
+    ImageVerify: typeof import('./../components/custom/ImageVerify.vue')['default']
     LoadingEmptyWrapper: typeof import('./../components/business/LoadingEmptyWrapper.vue')['default']
     LoginAgreement: typeof import('./../components/business/LoginAgreement.vue')['default']
     NaiveProvider: typeof import('./../components/common/NaiveProvider.vue')['default']
@@ -89,5 +96,7 @@ declare module '@vue/runtime-core' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SvgIcon: typeof import('./../components/custom/SvgIcon.vue')['default']
+    SystemLogo: typeof import('./../components/common/SystemLogo.vue')['default']
+    WebSiteLink: typeof import('./../components/custom/WebSiteLink.vue')['default']
   }
 }
